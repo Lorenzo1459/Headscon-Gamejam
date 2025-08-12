d65e495b717d0fdc52ed2941be03823f4aa0bf1e
Danilo Erler Lima <loklosgames@gmail.com>
2025-08-12 18:32:30 -0300

    GAMEJAM-VERSION

--- Assets/Scripts/Audio Managers/PlaySFX.cs
+++ Assets/Scripts/Audio Managers/PlaySFX.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlaySFX : MonoBehaviour
 {
     public AudioClip audioClip;
-    
+
+    public float volume = 1f; // Default volume, can be adjusted in the inspector   
     public void PlaySound()
     {
-        SFXManager.instance.PlaySFX(audioClip, transform, 1f);
+        SFXManager.instance.PlaySFX(audioClip, transform, volume);
     }
 }
